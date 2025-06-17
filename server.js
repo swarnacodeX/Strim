63033e9ae8e8caa5a50b4ea8ae8ccd0c3129252d
Shubhangan Das <shubhangandas7366@gmail.com>
2025-06-17 22:23:38 +0530

    changes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectDB(); // ✅ Call the function to connect
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST"],
   credentials: true
 }));
